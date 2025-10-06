133466f7a3066cef07c958915dfbd6f56f28a6d7
Ter <smarttapon.lim@gmail.com>
2025-10-07 04:25:14 +0700

    added lab

---
+++ try_it_out/src/opt3.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opt3.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slimvutt <slimvut@fpgij;dgj;ds.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 03:50:08 by slimvutt          #+#    #+#             */
+/*   Updated: 2025/10/05 03:50:08 by slimvutt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswap.h"
+
+void	ra(t_stack *a)
+{
+	t_node	*first;
+
+	if (a->size < 2)
+		return ;
+	first = a -> top;
+	a->top = first->next;
+	a->top->prev = NULL;
+	a->bottom->next = first;
+	first->prev = a->bottom;
+	first->next = NULL;
+	a->bottom = first;
+	ft_putstr_fd("ra\n", 1);
+}
+
+void	rb(t_stack *b)
+{
+	t_node	*first;
+
+	if (b->size < 2)
+		return ;
+	first = b -> top;
+	b->top = first->next;
+	b->top->prev = NULL;
+	b->bottom->next = first;
+	first->prev = b->bottom;
+	first->next = NULL;
+	b->bottom = first;
+	ft_putstr_fd("rb\n", 1);
+}
+
+void	rr(t_stack *a, t_stack *b)
+{
+	ra(a);
+	rb(b);
+	ft_putstr_fd("rr\n", 1);
+}
---
+++ try_it_out/src/pushswap.h
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pushswap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slimvutt <slimvut@fpgij;dgj;ds.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 09:24:01 by slimvutt          #+#    #+#             */
+/*   Updated: 2025/10/04 09:24:01 by slimvutt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSHSWAP_H
+# define PUSHSWAP_H
+# include "../libft/libft.h"
+# include <limits.h>
+//---------------------------------------------------------------
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	int				keep;
+	int				cost_a;
+	int				cost_b;
+	int				total_cost;
+	struct s_node	*target;
+	struct s_node	*next;
+	struct s_node	*prev;
+}				t_node;
+
+typedef struct s_stack
+{
+	t_node		*top;
+	t_node		*bottom;
+	int			size;
+}				t_stack;
+//---------------------------------------------------------------
+int				ft_check(t_stack *stack, char *nbr, int value);
+void			free_stack(t_stack *stack);
+t_node			*new_t(t_stack *stack, int nbr);
+t_stack			*init_a(void);
+t_stack			*init_stack_b(void);
+t_stack			*init_stack_a(char **args, int i);
+void			free_init_a_if_error(t_stack *stack);
+void			sa(t_stack *a);
+void			sb(t_stack *b);
+void			ss(t_stack *a, t_stack *b);
+void			pa(t_stack *a, t_stack *b);
+void			pb(t_stack *a, t_stack *b);
+void			ra(t_stack *a);
+void			rb(t_stack *b);
+void			rr(t_stack *a, t_stack *b);
+void			rra(t_stack *a);
+void			rrb(t_stack *b);
+void			rrr(t_stack *a, t_stack *b);
+//---------------------------------------------------------------
+void			print_stack(t_stack *stack, char name);
+#endif
